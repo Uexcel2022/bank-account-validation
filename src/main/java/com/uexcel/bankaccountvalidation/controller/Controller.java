920e0572e4af33baf05aa090af1581b1e4056fc7
Uexcel2022 <excellenceudoka@gmail.com>
2023-11-30 12:12:22 +0100

    commit

--- src/main/java/com/uexcel/bankaccountvalidation/controller/Controller.java
+++ src/main/java/com/uexcel/bankaccountvalidation/controller/Controller.java
@@ -7,17 +7,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Controller {
-    @GetMapping("/validate/{bankDetails}/{token}")
-    Object validatAccount(@PathVariable("bankDetails") String bankDetails, @PathVariable("token") String token)
+    @GetMapping("/validate**")
+    Object validatAccount(@RequestParam("account_number") String account_number,
+            @RequestParam("bank_code") String bank_code,
+            @RequestParam("token") String token)
             throws IOException {
 
-        String uri = "https://api.paystack.co/bank/resolve?" + bankDetails;
+        String uri = "https://api.paystack.co/bank/resolve?account_number=" + account_number + "&bank_code="
+                + bank_code;
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -27,7 +30,8 @@ public class Controller {
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(uri, header);
 
-        return restTemplate.exchange(uri, HttpMethod.GET, requestEntity, Object.class).getBody();
+        return restTemplate.exchange(uri, HttpMethod.GET, requestEntity,
+                Object.class).getBody();
 
     }
 }
